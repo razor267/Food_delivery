16bd5d175ce0a0bebc11a7b8eabae8d2073ec816
Petr Baiborin <razor267@mail.ru>
2021-12-22 19:49:21 +0300

    for, while, do, if, switch. LES 15

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-"use strict";
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
@@ -10,12 +24,45 @@ const personalMovieDB = {
     privat: false
 };
 
-const film1 = prompt('Один из последних просмотренных фильмов?', ""),
-    gradeFilm1 = prompt('На сколько оцените его?', ""),
-    film2 = prompt('Один из последних просмотренных фильмов?', ""),
-    gradeFilm2 = prompt('СНа сколько оцените его?', "");
+for (let i = 0; i < 2; i++) {
+    const film = prompt('Один из последних просмотренных фильмов?', ""),
+        gradefilm = prompt('На сколько оцените его?', "");
+    if (film === '' || film.length > 50 || gradefilm === '' || film == null || gradefilm == null) {
+        i--;
+        continue;
+    }
+    personalMovieDB.movies[film] = gradefilm;
+}
 
-personalMovieDB.movies[film1] = gradeFilm1;
-personalMovieDB.movies[film2] = gradeFilm2;
+/*let i=0;
+do {
+    const film = prompt('Один из последних просмотренных фильмов?', ""),
+        gradefilm = prompt('На сколько оцените его?', "");
+    if (film === '' || film.length > 50 || gradefilm === '') {
+        continue;
+    }
+    personalMovieDB.movies[film] = gradefilm;
+    i++
+} while (i<2);*/
+
+/*while (i<2) {
+    const film = prompt('Один из последних просмотренных фильмов?', ""),
+        gradefilm = prompt('На сколько оцените его?', "");
+    if (film === '' || film.length > 50 || gradefilm === '') {
+        continue;
+    }
+    personalMovieDB.movies[film] = gradefilm;
+    i++
+}*/
+
+if (numberOfFilms < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms <= 30) {
+    console.log('Вы классический зритель');
+} else if (numberOfFilms > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
