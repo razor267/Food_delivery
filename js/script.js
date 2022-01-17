626921173ca5d18a53dac0a40016027783485906
Petr Baiborin <razor267@mail.ru>
2022-01-17 14:38:47 +0300

    create modules. LES 73

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
+import calc from './modules/calc';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import modal, {openModal} from './modules/modal';
+import slider from './modules/slider';
+import tabs from './modules/tabs';
+import timer from './modules/timer';
+
 document.addEventListener('DOMContentLoaded', () => {
-    const calc = require('./modules/calc'),
-        cards = require('./modules/cards'),
-        forms = require('./modules/forms'),
-        modal = require('./modules/modal'),
-        slider = require('./modules/slider'),
-        tabs = require('./modules/tabs'),
-        timer = require('./modules/timer');
+
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
 
     calc();
     cards();
-    forms();
-    modal();
-    slider();
-    tabs();
-    timer();
+    forms('form', modalTimerId);
+    modal('[data-modal]', '.modal', modalTimerId);
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    timer('.timer', '2022-01-31');
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        slide: '.offer__slide',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
 });
-
-
-
-
-
-
